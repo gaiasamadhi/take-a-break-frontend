e11c951a03d249c45bc8d3487b52640d0b8f2175
mihail <mihail.soltan@protonmail.ch>
2021-10-20 18:25:34 +0200

    finish basic layout, fix timer

--- src/pages/Activity.js
+++ src/pages/Activity.js
@@ -1,6 +1,18 @@
 import "../Activity.css";
+import Countdown, { zeroPad } from "react-countdown";
+import activityPic from "../assets/coffee-cup.png";
+import marker from "../assets/Photo1.png";
+import background from "../assets/Photo5.png";
+import { useRef } from "react";
 export default function Activity() {
-  console.log("lmao");
+  const timerRef = useRef();
+  const handleStart = () => timerRef.current.start();
+  const handlePause = () => timerRef.current.pause();
+  const renderer = ({ minutes, seconds }) => (
+    <span>
+      {zeroPad(minutes)}:{zeroPad(seconds)}
+    </span>
+  );
   return (
     <div className="activityPage">
       <div className="restart-break">
@@ -19,11 +31,50 @@ export default function Activity() {
         ></iframe>
         <div className="timer">
           <div className="clock">
-            <h1>5:00</h1>
+            <h1>
+              <Countdown
+                autoStart={false}
+                ref={timerRef}
+                renderer={renderer}
+                date={Date.now() + 300000}
+              />
+            </h1>
           </div>
-          <button className="start-button">Start timer</button>
-          <button className="end-button">End activity early</button>
+          <button className="start-button" onClick={handleStart}>
+            Start timer
+          </button>
+          <button className="end-button" onClick={handlePause}>
+            End activity early
+          </button>
         </div>
+      </div>
+      <div className="break">
+        <div className="breakDescriptionContainer">
+          <h2>Coffee Break - 5 Minutes</h2>
+          <div className="activityDescription">
+            <img src={activityPic}></img>
+            <p>
+              Take a break and go grab or get ready to make your favourite cup
+              of coffee!
+            </p>
+          </div>
+        </div>
+        <div className="breakDescriptionContainer">
+          <h2>Benefits of Activity</h2>
+          <div className="benefitDescription">
+            
+              <div><img src={marker}></img><p>Improvement in energy levels & reaction times</p></div>
+              <div><img src={marker}></img><p>Help metabolism burn fat</p></div>
+              <div><img src={marker}></img><p>Improve physical performance</p></div>
+           
+          </div>
+        </div>
+      </div>
+      <div className="bg">
+        <img
+          src={background}
+         
+        ></img>
       </div>
     </div>
   );
