81ceeaf7befb944658216be8a343ff5869feda90
gaiasamadhi <64321211+gaiasamadhi@users.noreply.github.com>
2021-10-25 15:04:42 +0200

    Added new formatting to SymptomsMain

--- src/containers/browse-symptoms.js
+++ src/containers/browse-symptoms.js
@@ -24,12 +24,54 @@ const BrowseSymptomsContainer = () => {
                     setErrMessage(err.message);
                     setFetchStatus('FAILED');
                 })
+        setListSymptoms(symptoms.map((entry, idx) => [entry.name]));
+        console.log('listSymptoms',listSymptoms)
+   
+               
         }, []);
-
+        
+        
+        const renderSymptoms = () => {
+            let tempOne =[]
+            let tempTwo =[]
+            let tempThree = []
+            let tempFour = []
+            let tempFive =[]
+            let tempSix =[]
+            const temp = symptoms.forEach((entry, idx) => {    
+                    if(idx<3) {
+                        tempOne.push(entry.name)
+                    }
+                    if(3<=idx<6) {
+                        tempTwo.push(entry.name)
+                    }
+                    if(6<=idx<8) {
+                        tempThree.push(entry.name)
+                    }
+                    if(8<=idx<10) {
+                        tempFour.push(entry.name)
+                    }
+                    if(10<=idx<12) {
+                        tempFive.push(entry.name)
+                    }
+                return tempOne.concat(tempTwo)     
+                });
+            console.log('tempOne', tempOne)
+            console.log('tempTwo', tempTwo)
+            console.log('temp',temp)  
+            console.log('symptoms', symptoms) 
+            return (
+               temp.map((ele) => (
+                <div className='browse-symptoms-main-row'>
+                    {ele.map((entry,idx) => <div className='browse-symptoms-main-row__checkbox'><input className='browse-symptoms-main-row__input' type='checkbox' name={entry.name} value={entry} id={idx}></input><label className='browse-symptoms-main-row__label' for={entry}>{entry}</label></div>)}
+                </div>    
+               ))     
+            )
+        };
   return (
-    <div className='browse-symptoms-main-row'>      
-        {symptoms.map((entry, idx) => <div className='browse-symptoms-main-row__checkbox'><input className='browse-symptoms-main-row__input' type='checkbox' name={entry.name} value={entry._id} id={entry.name}></input><label className='browse-symptoms-main-row__label' for={entry.name}>{entry.name}</label></div>)}
-    </div>
+    <>     
+        {renderSymptoms()}
+    </>
   )
 };
 
